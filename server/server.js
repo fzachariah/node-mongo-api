1815650845ff690f4f62e96146a13e3001de1692
FEBIN ZACHARIAH <febinzachariah6@gmail.com>
2017-07-29 17:52:27 -0400

    Post Todos Route

---
+++ server/server.js
@@ -0,0 +1,37 @@
+var express =require('express');
+var bodyParser=require('body-parser');
+var app=express();
+
+const port=process.env.PORT || 3000;
+
+
+var {mongoose} =require('./db/mongoose');
+var {Todo}=require('./models/todo');
+var {User}=require('./models/user');
+
+app.use(bodyParser.json());
+
+app.post('/todos',function(req,res){
+
+    var toDo=new Todo({
+        text:req.body.text
+    });
+
+    toDo.save().then((doc) =>{
+
+        res.send(doc);
+    }, (e) =>{
+
+        res.status(400).send(e);
+
+    });
+
+});
+
+
+
+app.listen(port,function(){
+
+    console.log('Server is Up at Port No: '+port);
+
+});
