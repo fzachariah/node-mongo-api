305149e74a1484144d93a75d166c88e579593ddd
FEBIN ZACHARIAH <febinzachariah6@gmail.com>
2017-07-30 00:49:55 -0400

    'Add-Get-Todos-Id'

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express =require('express');
 var bodyParser=require('body-parser');
+const {ObjectID}=require('mongodb');
 var app=express();
 
 const port=process.env.PORT || 3000;
@@ -37,6 +38,30 @@ app.get('/todos',function(req,res){
         res.status(400).send(e);
 
     });
+});
+
+
+app.get('/todos/:id',function(req,res){
+
+    var id=req.params.id;
+
+    if(!ObjectID.isValid(id))
+    {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) =>{
+
+        if(!todo)
+        {
+            return res.status(404).send();
+        }
+        
+        res.send({todo});
+
+    }).catch((e) =>{
+        res.status(404).send();
+    })
 });
 
 
